83a648216da90a8cc52b423570a084748a5b2903
hachiper <71579318+hachiper@users.noreply.github.com>
2024-08-09 21:30:23 +0800

    Add files via upload

---
+++ scripts/long_term_forecast/ETT_script/QURNN_ETTm1.sh
@@ -0,0 +1,116 @@
+model_name=QURNN
+
+python -u prediction_for_qurnn.py  \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --root_path ./dataset/ETT-small/ \
+        --data_path ETTm1.csv \
+        --model_id ETTm1_96_96 \
+        --model QURNN \
+        --data ETTm1 \
+        --features M \
+        --seq_len 96 \
+        --label_len 48 \
+        --pred_len 96  \
+        --e_layers 2 \
+        --d_layers 1 \
+        --factor 3 \
+        --enc_in 7 \
+        --dec_in 7 \
+        --c_out 7 \
+        --d_model 16 \
+        --d_ff 32 \
+        --des 'Exp' \
+        --itr 1 \
+        --top_k 5 \
+        --activation gelu  \
+        --learning_rate 0.001 \
+        --hidden_dim '[64]' \
+        --cell_dim '[64]'
+
+python -u prediction_for_qurnn.py  \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --root_path ./dataset/ETT-small/ \
+        --data_path ETTm1.csv \
+        --model_id ETTm1_96_192 \
+        --model QURNN \
+        --data ETTm1 \
+        --features M \
+        --seq_len 96 \
+        --label_len 48 \
+        --pred_len 192  \
+        --e_layers 2 \
+        --d_layers 1 \
+        --factor 3 \
+        --enc_in 7 \
+        --dec_in 7 \
+        --c_out 7 \
+        --d_model 16 \
+        --d_ff 32 \
+        --des 'Exp' \
+        --itr 1 \
+        --top_k 5 \
+        --activation gelu  \
+        --learning_rate 0.001 \
+        --hidden_dim '[64]' \
+        --cell_dim '[64]'
+
+python -u prediction_for_qurnn.py  \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --root_path ./dataset/ETT-small/ \
+        --data_path ETTm1.csv \
+        --model_id ETTm1_96_336 \
+        --model QURNN \
+        --data ETTm1 \
+        --features M \
+        --seq_len 96 \
+        --label_len 48 \
+        --pred_len 336  \
+        --e_layers 2 \
+        --d_layers 1 \
+        --factor 3 \
+        --enc_in 7 \
+        --dec_in 7 \
+        --c_out 7 \
+        --d_model 16 \
+        --d_ff 32 \
+        --des 'Exp' \
+        --itr 1 \
+        --top_k 5 \
+        --activation gelu  \
+        --learning_rate 0.001 \
+        --hidden_dim '[64]' \
+        --cell_dim '[64]'
+
+
+python -u prediction_for_qurnn.py  \
+        --task_name long_term_forecast \
+        --is_training 1 \
+        --root_path ./dataset/ETT-small/ \
+        --data_path ETTm1.csv \
+        --model_id ETTm1_96_720 \
+        --model QURNN \
+        --data ETTm1 \
+        --features M \
+        --seq_len 96 \
+        --label_len 48 \
+        --pred_len 720  \
+        --e_layers 2 \
+        --d_layers 1 \
+        --factor 3 \
+        --enc_in 7 \
+        --dec_in 7 \
+        --c_out 7 \
+        --d_model 16 \
+        --d_ff 32 \
+        --des 'Exp' \
+        --itr 1 \
+        --top_k 5 \
+        --activation gelu  \
+        --learning_rate 0.001 \
+        --hidden_dim '[64]' \
+        --cell_dim '[64]'
+
+
